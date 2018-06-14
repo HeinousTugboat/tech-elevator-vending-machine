5886da07923718be70e6c5ed31088e9fad5a1e92
Alexander Jarman <sascha.jarman@gmail.com>
2018-06-14 13:57:02 -0400

    adds initial ConsoleManager

---
+++ c#-mini-capstone/Capstone/Classes/ConsoleManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    interface IConsoleManager
+    {
+        decimal CurrentBalance { get; set; }
+        VendingMachineItem CurrentSelection { get; }
+
+        UIAction PrintMainMenu();
+        UIAction PrintPurchasingMenu();
+        UIAction PrintProductSelectionMenu(Dictionary<ItemType, VendingMachineItem> items);
+
+        // Internal Utility Functions
+        // ResetDisplay..
+        // Print Product thing..
+
+    }
+
+    public class ConsoleManager
+    {
+        // WHAT DOES YOU MANAGE?
+        // Console. Duh.
+        // Print Main Menu
+        //   Display Items
+        //   Purchase Item
+        //   Exit
+        // Print Purchasing Menu
+        //   Feed Money
+        //   Select Product
+        //   Finish Transaction
+        // Print Product Selection Menu
+        //   Print all slots?
+
+        // Print Messages such as 
+        //   Out of Stock 
+        //   Shit's Broked
+        //   Dispensary Commentary ("Crunch Crunch, Yum!")..
+        //   Product Code doesn't exist.. if we use menu, not necessary? Just don't show unavailable stuff..
+        
+        // Current Balance!
+
+    }
+}
