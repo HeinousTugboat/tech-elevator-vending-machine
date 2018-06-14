304593da8b50dba881dac26a84a4f7032a7eb2eb
Alexander Jarman <sascha.jarman@gmail.com>
2018-06-14 11:34:22 -0400

    adds initial VendingMachineItem

--- c#-mini-capstone/Capstone/Classes/VendingMachineItem.cs
+++ c#-mini-capstone/Capstone/Classes/VendingMachineItem.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Capstone.Classes
 {
-    class VendingMachineItem
+    public enum ItemType
     {
+        Chip = 'A',
+        Candy = 'B',
+        Drink = 'C',
+        Gum = 'D'
+    }
+    public class VendingMachineItem
+    {
+        public ItemType Type { get; set; }
+        public int Slot { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Name { get; set; }
+
+        public VendingMachineItem(string name, decimal price, ItemType type, int slot)
+        {
+            Name = name;
+            Type = type;
+            Slot = slot;
+            Price = price;
+            Quantity = 5;
+        }
+
+        public override string ToString()
+        {
+            return $"{(char)Type}{Slot} | {Name} | {Price.ToString("C")} | {Quantity} | {Type}";
+        }
     }
 }
