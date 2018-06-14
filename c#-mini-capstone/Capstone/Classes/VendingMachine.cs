29a709860f79eb7db5c879336f4ac13ee0fbdb86
Alexander Jarman <sascha.jarman@gmail.com>
2018-06-14 12:44:15 -0400

    adds IVendingMachine and preliminary stuff

--- c#-mini-capstone/Capstone/Classes/VendingMachine.cs
+++ c#-mini-capstone/Capstone/Classes/VendingMachine.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Capstone.Classes
 {
-    public class VendingMachine
+    interface IVendingMachine
     {
+        // Functions that touch CurrentBalance
+        VendingMachineTransaction FeedMoney(int amountToAdd);
+        VendingMachineTransaction FinishTransaction();
+        VendingMachineTransaction PurchaseItem(ItemType type, int slot);
+
+        // Functions for Data Manager or User Interface to call
+        VendingMachineItem CheckItem(ItemType type, int slot);
+        Dictionary<ItemType, VendingMachineItem[]> GetAllItems();
+        List<VendingMachineTransaction> GetAllTransactions();
+    }
+
+    public class VendingMachine //: IVendingMachine
+    {
+        private Dictionary<ItemType, VendingMachineItem[]> StockList { get; }
+        private List<VendingMachineTransaction> TransactionLog { get; }
+        public decimal CurrentBalance { get; private set; }
+
+        //public VendingMachine() : this(new List<VendingMachineItem>()) { }
+
+        public VendingMachine(List<VendingMachineItem> list)
+        {
+            // Assign items to StockList
+        }
+
     }
 }
