1f1216ec4cae4f5f4bc872cdea685109cb2e974e
Alexander Jarman <sascha.jarman@gmail.com>
2018-06-14 14:47:21 -0400

    adds basic Main Menu functionality

--- c#-mini-capstone/Capstone/Classes/ConsoleManager.cs
+++ c#-mini-capstone/Capstone/Classes/ConsoleManager.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+using static System.ConsoleColor;
 
 namespace Capstone.Classes
 {
-    interface IConsoleManager
+    interface IUIManager
     {
         decimal CurrentBalance { get; set; }
         VendingMachineItem CurrentSelection { get; }
@@ -14,15 +16,156 @@ namespace Capstone.Classes
         UIAction PrintMainMenu();
         UIAction PrintPurchasingMenu();
         UIAction PrintProductSelectionMenu(Dictionary<ItemType, VendingMachineItem> items);
-
-        // Internal Utility Functions
-        // ResetDisplay..
-        // Print Product thing..
-
     }
 
-    public class ConsoleManager
+    public class ConsoleManager : IUIManager
     {
+        // Used to pass in the balance from the UI class.
+        public decimal CurrentBalance { get; set; }
+
+        // Used to pass whatever's currently selected *out* to the UI.
+        public VendingMachineItem CurrentSelection { get; private set; }
+
+        // Default window colors, can be whatever. Normally White on Black.
+        private ConsoleColor baseFG = White;
+        private ConsoleColor baseBG = Black;
+
+        public ConsoleManager()
+        {
+            // Setting our console colors to our default ones.
+            BackgroundColor = baseBG;
+            ForegroundColor = baseFG;
+
+            // Resetting the buffer and window so that we 
+            //   a.) don't have any scroll bar and 
+            //   b.) don't have any excess space
+            BufferWidth = WindowWidth;
+            BufferHeight = WindowHeight;
+            SetWindowSize(BufferWidth, BufferHeight);
+
+            // Hide that ugly little cursor. We'll do it ourselves.
+            CursorVisible = false;
+            Title = "Vend-o-matic 600";
+
+            // Reset the console window so our formatting will take effect across the whole thing
+            Clear();
+            SetCursorPosition(0, 0);
+            PrintBalance();
+        }
+
+        private void PrintBalance()
+        {
+            CursorTop = WindowHeight - 1;
+            string balance = "Currently Available: " + CurrentBalance.ToString("C");
+            CursorLeft = WindowWidth - balance.Length - 1;
+            Write(balance);
+            SetCursorPosition(0, 0);
+        }
+
+        // Sets current color based on BaseBG/FG.
+        private void SetColor(bool invert = false)
+        {
+            if (invert)
+            {
+                ForegroundColor = baseBG;
+                BackgroundColor = baseFG;
+            }
+            else
+            {
+                ForegroundColor = baseFG;
+                BackgroundColor = baseBG;
+            }
+        }
+
+        private void MainMenuOutput(int selectedOption = 0)
+        {
+            SetCursorPosition(2, 2);
+            SetColor(selectedOption == 0);
+            Write("(1) Display Vending Machine Items ");
+
+            SetCursorPosition(2, 3);
+            SetColor(selectedOption == 1);
+            Write("(2) Purchase ");
+
+            SetCursorPosition(2, 4);
+            SetColor(selectedOption == 2);
+            Write("(3) Exit ");
+
+            SetColor();
+        }
+
+        public UIAction PrintMainMenu()
+        {
+            int selectedOption = 0;
+            bool inMenu = true;
+            ConsoleKeyInfo keyPress;
+
+            while (inMenu)
+            {
+                MainMenuOutput(selectedOption);
+                keyPress = ReadKey(true);
+
+                switch (keyPress.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selectedOption--;
+                        if (selectedOption < 0)
+                        {
+                            selectedOption = 2;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selectedOption++;
+                        if (selectedOption > 2)
+                        {
+                            selectedOption = 0;
+                        }
+                        break;
+                    case ConsoleKey.NumPad1:
+                    case ConsoleKey.D1:
+                        selectedOption = 0;
+                        inMenu = false;
+                        break;
+                    case ConsoleKey.NumPad2:
+                    case ConsoleKey.D2:
+                        selectedOption = 1;
+                        inMenu = false;
+                        break;
+                    case ConsoleKey.NumPad3:
+                    case ConsoleKey.D3:
+                        selectedOption = 2;
+                        inMenu = false;
+                        break;
+                    case ConsoleKey.Enter:
+                    case ConsoleKey.Escape:
+                    case ConsoleKey.Spacebar:
+                        inMenu = false;
+                        break;
+                }
+            }
+
+            PrintBalance();
+            if (selectedOption == 0)
+            {
+                return UIAction.DisplayItems;
+            }
+            if (selectedOption == 1)
+            {
+                return UIAction.PurchaseItem;
+            }
+            return UIAction.Exit;
+        }
+
+        public UIAction PrintPurchasingMenu()
+        {
+            return UIAction.Exit;
+        }
+
+        public UIAction PrintProductSelectionMenu(Dictionary<ItemType, VendingMachineItem> items)
+        {
+            return UIAction.Exit;
+        }
+
         // WHAT DOES YOU MANAGE?
         // Console. Duh.
         // Print Main Menu
@@ -41,7 +184,7 @@ namespace Capstone.Classes
         //   Shit's Broked
         //   Dispensary Commentary ("Crunch Crunch, Yum!")..
         //   Product Code doesn't exist.. if we use menu, not necessary? Just don't show unavailable stuff..
-        
+
         // Current Balance!
 
     }
--- c#-mini-capstone/Capstone/Classes/UserInterface.cs
+++ c#-mini-capstone/Capstone/Classes/UserInterface.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace Capstone.Classes
 {
-    enum UIAction
+    public enum UIAction
     {
+        MainMenu, // User viewing the main menu
         FeedMoney, // User wants to add money
         DisplayItems, // User wants to see product selection grid
         SelectProduct, // User has selected a product on the UI
@@ -19,20 +20,44 @@ namespace Capstone.Classes
     public class UserInterface
     {
         private VendingMachine vendingMachine;
+        private IUIManager uiManager;
 
         public UserInterface(VendingMachine vendingMachine)
         {
-
+            uiManager = new ConsoleManager();
         }
 
         public void RunInterface()
         {
             bool done = false;
+            UIAction action = UIAction.MainMenu;
+
             while (!done)
             {
-                Console.WriteLine("This is the UserInterface");
-                Console.ReadLine();
+                uiManager.CurrentBalance += 0.01M;
+
+                switch (action)
+                {
+                    case UIAction.MainMenu:
+                        action = uiManager.PrintMainMenu();
+                        break;
+                    case UIAction.Exit:
+                        done = true;
+                        break;
+                    case UIAction.PurchaseItem:
+                        action = uiManager.PrintPurchasingMenu();
+                        break;
+                    case UIAction.DisplayItems:
+                        //action = uiManager.PrintProductSelectionMenu()
+                        //break;
+                    default:
+                        action = UIAction.MainMenu;
+                        break;
+
+                }
+                Console.SetCursorPosition(1, Console.WindowHeight - 1);
             }
+            Console.Write("Last Action Taken: " + action + "\t\t");
 
         }
 
