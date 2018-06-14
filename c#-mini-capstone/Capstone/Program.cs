3412242819d404fbed23337ab5465f2ac8837ec6
Alexander Jarman <sascha.jarman@gmail.com>
2018-06-14 13:58:29 -0400

    updates vending machine to construct empty list

--- c#-mini-capstone/Capstone/Program.cs
+++ c#-mini-capstone/Capstone/Program.cs
@@ -11,9 +11,12 @@ namespace Capstone
     {
         static void Main(string[] args)
         {
-            VendingMachine vendingMachine = new VendingMachine();
+            VendingMachine vendingMachine = new VendingMachine(new List<VendingMachineItem> { });
             UserInterface userInterface = new UserInterface(vendingMachine);
             userInterface.RunInterface();
+
+
+
         }
     }
 }
